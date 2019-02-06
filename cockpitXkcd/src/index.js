86130cae887c3fc87dac51de547dcee662b09a74
Johannes Heinemann <johannes.heinemann@camunda.com>
2019-02-06 13:56:21 +0100

    Add xkcd comic to cockpit

---
+++ cockpitXkcd/src/index.js
@@ -0,0 +1,16 @@
+define(["angular"], function(angular) {
+  var ngModule = angular.module("cockpit.cockpitXkcd", []);
+
+  ngModule.config([
+    "ViewsProvider",
+    function(ViewsProvider) {
+      ViewsProvider.registerDefaultView("cockpit.dashboard", {
+        id: "cockpit.cockpitXkcd",
+        priority: 9001,
+        template: `<center><img src="https://imgs.xkcd.com/comics/tree.png "></center>`
+      });
+    }
+  ]);
+
+  return ngModule;
+});
